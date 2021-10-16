c53e6568035d1d3f566f439a02536292d303ddfb
Soham <sohamy11@gmail.com>
2021-10-16 22:58:47 +0530

    Added some design to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import './Footer.css';
-import fblogo from '../images/fb-logo.png';
-import linkedinlogo from '../images/linkedin-logo.png';
-import ytlogo from '../images/yt-logo.png';
-import instalogo from '../images/ig-logo.png';
-
+import enabled from '../images/for-the-en-abled.jpg';
 
 function Footer() {
     const [click, setClick] = useState(false);
@@ -18,40 +14,34 @@ function Footer() {
         <nav className="footer">
             <div className="footer-top">
                 <div className="footer-first">
-                    <Link to="/" className="footer-logo" onClick={closeMobileMenu}>
+                    {/* <Link to="/" className="footer-logo" onClick={closeMobileMenu}>
                         Abilitare
+                    </Link> */}
+                    <Link to="/" className="footer-logo" onClick={closeMobileMenu}>
+                        <img src={enabled} width="200"/>
                     </Link>
                 </div>
                 <div className="footer-second">
-                    <div className="follow-us">
-                        Follow Us
-                    </div>
-                    <a href="https://www.facebook.com/">
-                        <img className="social-logo" src={fblogo} width='35' />
+                    <a href="https://twitter.com/Abilitare">
+                        <i id="social-logos" class="fab fa-twitter fa-lg"></i>
+                    </a>
+                    <a href="https://www.facebook.com/AbilitareInc/">
+                        <i id="social-logos" class="fab fa-facebook-f fa-lg"></i>
                     </a>
                     <a href="https://www.linkedin.com/company/abilitareinc/">
-                        <img className="social-logo" src={linkedinlogo} width='35' />
+                        <i id="social-logos" class="fab fa-linkedin-in fa-lg"></i>
                     </a>
-                    {/* <a href="https://www.youtube.com/">
-                        <img className="social-logo" src={ytlogo} width='75' />
-                    </a> */}
-                    <a href="https://www.instagram.com/">
-                        <img className="social-logo" src={instalogo} width='35' />
+                    <a href="https://www.youtube.com/watch?v=fPDMhJKK-QY">
+                        <i id="social-logos" class="fab fa-youtube fa-lg"></i>
+                    </a>
+                    <a href="https://www.instagram.com/abilitare/">
+                        <i id="social-logos" class="fab fa-instagram fa-lg"></i>
                     </a>
                 </div>
                 <div className="footer-third">
-                    <div className="footer-contact">
-                        <div className="follow-us2">
-                            Contact
-                        </div>
-                        <div className="mail-ids2">
-                            <p><i class="fa fa-envelope" aria-hidden="true"></i> contact@abilitare.com</p>
-                            <p><i class="fa fa-phone fa-rotate-90" aria-hidden="true"></i> +1 352-327-2284</p>
-                        </div>
-                    </div>
-                    <br />
-                    <div className="footer-address">
-                        <div className="follow-us3">
+
+                <div className="footer-address">
+                        <div className="follow-us">
                             Address
                         </div>
                         <div className="main-address">
@@ -59,11 +49,22 @@ function Footer() {
                                 Gainesville, Florida 32601, US</p>
                         </div>
                     </div>
+                    <div className="footer-contact">
+                        <div className="follow-us">
+                            Contact Us
+                        </div>
+                        <div className="mail-ids2">
+                            <p><i class="fa fa-envelope" aria-hidden="true"></i> info@abilitare.com</p>
+                            <p><i class="fa fa-phone fa-rotate-90" aria-hidden="true"></i> +1 352-327-2284</p>
+                        </div>
+                    </div>
+                    
+                    
                 </div>
             </div>
             <div className="footer-bottom">
                 <div className="copyright">
-                 Abilitare Inc. &copy; 2021
+                    Abilitare Inc &copy; 2021
                 </div>
             </div>
         </nav>
