8fdee849f81587aff590333854e00b69ffb4a958
Soham <sohamy11@gmail.com>
2021-11-23 23:57:23 +0530

    Toolbar section

--- src/components/Home/components/DownloadSection.js
+++ src/components/Home/components/DownloadSection.js
@@ -3,13 +3,23 @@ import "../Assets/CSS/DownloadSection.css";
 import toolbar from "../../../images/toolbar.png";
 import desktopclient from "../../../images/desktop-client.png";
 import { Button } from "./Button";
+import leftclick from "../../../images/left-click.gif";
+import rightclick from "../../../images/right-click.gif";
+import doubleclick from "../../../images/double-click.gif";
+import scroll from "../../../images/scroll.gif";
+import pin from "../../../images/pin.gif";
+import settings from "../../../images/settings.gif";
+import none from "../../../images/none.gif";
+import clickanddrag from "../../../images/click-and-drag.gif";
+
 
 
 function DownloadSection() {
     return (
+        <>
         <div className="main">
             <div className="left">
-                <img src={toolbar} alt="" />
+                <img src="" alt="" />
             </div>
             <div className="right">
                 <h1>Abili Toolbar</h1>
@@ -34,6 +44,18 @@ function DownloadSection() {
                 </div>
             </div>
         </div>
+        <div className="how-to">
+            <h1>How To Use?</h1>
+            <div className="single-section">
+                <div className="left-gif">
+                    <img src={leftclick} alt="" />
+                </div>
+                <div className="right-info">
+                    <h1>Left Click</h1>
+                </div>
+            </div>
+        </div>
+        </>
     )
 }
 
